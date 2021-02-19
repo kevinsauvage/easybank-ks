9bb4f5b3aea00bff586709b9ad402827ab886b79
kevin sauvage <kevinsauvage@outlook.com>
2021-02-19 17:04:49 +0100

    adding fade in on buttoon

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Button.css";
+import Fade from "react-reveal/Fade";
 
 const Button = () => {
   return (
-    <button className="button flex-jc-c-ai-c">
-      <p>Request Invite</p>
-    </button>
+    <Fade bottom cascade>
+      <button className="button flex-jc-c-ai-c">
+        <p>Request Invite</p>
+      </button>
+    </Fade>
   );
 };
 
